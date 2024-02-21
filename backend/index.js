494ab7a37a2fcb1b97b91e736f8f9e55bdecc907
ruwaruwa <almozali287@gmail.com>
2024-02-21 12:07:52 +0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express=require('express')
+const app=express()
+const dbConnection=require('./database/DBconnection')
+//db connections
+dbConnection()
+app.listen(1000,async()=>{
+    console.log('started !')
+})
+app.get('/',async(req,res)=>{
+    res.send('app started !!!!!')
+})
---
+++ frontend/src/components/pages/Students.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import SideNav from '../dashbourd/SideNav'
+
+export default function Students() {
+  return (
+    <div className=''>
+          {/* <SideNav/> */}
+    <div className='bg-red-600 p-0'>
+       
+        <div className='p-4 sm:ml-[20%] ml-[26%]'>
+        <h1 className='text-5xl text-black'>Students</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet deleniti repellat fuga atque incidunt dicta cum! Hic pariatur ipsum veniam, illum minus est distinctio
+             ad, blanditiis magnam eum consequatur laboriosam.</p>
+             <h1 className='text-5xl text-black'>Students</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet deleniti repellat fuga atque incidunt dicta cum! Hic pariatur ipsum veniam, illum minus est distinctio
+             ad, blanditiis magnam eum consequatur laboriosam.</p>
+        </div>
+    </div>
+    </div>
+  )
+}
